e2b86d255475fd1f18a843ed031af7239843ab40
facugody12 <facugodoy1205@gmail.com>
2025-11-13 12:03:22 -0300

    vistas productos con stock y pedidos por cliente

--- Base de Datos/Vistas.sql
+++ Base de Datos/Vistas.sql
@@ -12,3 +12,19 @@ FROM Pedido p
 JOIN DetalleDelPedido dp ON p.id_pedido = dp.id_pedido
 GROUP BY p.id_pedido, p.id_cliente;
 GO
+
+--vista productos con stock:
+CREATE VIEW vw_ProductosConStock
+AS
+SELECT PR.nombre_proveedor AS Proveedor, P.nombre AS Cliente, P.precio, S.cantidad FROM Proveedor PR
+INNER JOIN Producto P ON PR.id_proveedor = P.id_proveedor
+INNER JOIN Stock S ON P.id_producto = S.id_producto
+WHERE S.cantidad >= 0;
+go
+
+--vista pedidos clientes:
+CREATE VIEW vw_PedidosCliente
+AS
+SELECT C.nombre + ' ' + C.apellido AS Cliente, PE.fecha_pedido, PE.estado, PE.total FROM Cliente C
+INNER JOIN Pedido PE ON C.id_cliente = PE.id_cliente;
+go
